7334a7da5e366316f6cf8b23b65c0446e00bcddd
alexpasmantier <alex.pasmant@gmail.com>
2024-11-19 13:42:50 +0100

    fix step order

--- Formula/television.rb
+++ Formula/television.rb
@@ -5,12 +5,12 @@ class Television < Formula
   sha256 "7f77ebb2f5bb7d49092373f652070ff4c9b63f389b512f3a60b0d0e27fb42fa1"
   license "MIT"
 
-  depends_on "rust" => :build
-
   bottle do
     rebuild 1
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "a72c29f504a8fa91eedaeef12af0ee3c5afdbe72efb3150cf759ecde740b25f3"
   end
+
+  depends_on "rust" => :build
 
   def install
     system "cargo", "install", *std_cargo_args
